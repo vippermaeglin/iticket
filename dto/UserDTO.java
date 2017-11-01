1626c8f0f222683e3b537bfb01436fbf1cda1951
Vinícius Arruda <vinicius.silva.arruda@gmail.com>
2017-11-01 00:24:46 -0200

    Update UserDTO.java

--- dto/UserDTO.java
+++ dto/UserDTO.java
@@ -10,251 +10,142 @@ import java.util.List;
  *
  */
 @JsonInclude(Include.NON_NULL)
-public class UserDTO extends UserSimpleDTO{
-
-    // super: id, name, cpf
-    private Date createdAt = null;
-    private Date bornDate = null;
-    private String email = null;
-    private String cellphone = null;
-    private String facebookId = null;
-    private String gplusId = null;
-    private String gender = null;
-    private String pushDeviceToken = null;
-    private String deviceType = null;
-    private Integer fidelityPoints = null;
+public class UserDTO{
+    protected String id = null;
+    protected String name = null;
+    protected String cpf = null;
+    protected Date createdAt = null;
+    protected Date bornDate = null;
+    protected String email = null;
+    protected String cellphone = null;
+    protected String facebookId = null;
+    protected String gplusId = null;
+    protected String gender = null;
+    protected String pushDeviceToken = null;
+    protected String deviceType = null;
+    protected Integer fidelityPoints = null;
     @JsonIgnore
-    private String avatar = null;
+    protected String avatar = null;
     @JsonIgnore
-    private Integer dailyPoints = null;
+    protected Integer dailyPoints = null;
 
-    /**
-     * 
-     */
-    public UserDTO(){
-    	
+    public String getId() {
+        return id;
     }
-    
-	/**
-	 * @return the id
-	 */
-	public String getId() {
-		return id;
-	}
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * @return the createdAt
-	 */
-	public Date getCreatedAt() {
-		return createdAt;
-	}
+    public String getCpf() {
+        return cpf;
+    }
 
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
-	/**
-	 * @param createdAt the createdAt to set
-	 */
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
+    public Date getCreatedAt() {
+        return createdAt;
+    }
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 
+    public Date getBornDate() {
+        return bornDate;
+    }
 
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setBornDate(Date bornDate) {
+        this.bornDate = bornDate;
+    }
 
+    public String getEmail() {
+        return email;
+    }
 
-	/**
-	 * @return the bornDate
-	 */
-	public Date getBornDate() {
-		return bornDate;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
+    public String getCellphone() {
+        return cellphone;
+    }
 
-	/**
-	 * @param bornDate the bornDate to set
-	 */
-	public void setBornDate(Date bornDate) {
-		this.bornDate = bornDate;
-	}
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
 
+    public String getFacebookId() {
+        return facebookId;
+    }
 
-	/**
-	 * @return the cpf
-	 */
-	public String getCpf() {
-		return cpf;
-	}
+    public void setFacebookId(String facebookId) {
+        this.facebookId = facebookId;
+    }
 
+    public String getGplusId() {
+        return gplusId;
+    }
 
-	/**
-	 * @param cpf the cpf to set
-	 */
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public void setGplusId(String gplusId) {
+        this.gplusId = gplusId;
+    }
 
-	/**
-	 * @return the fidelityPoints
-	 */
-	public Integer getFidelityPoints() {
-		return fidelityPoints;
-	}
+    public String getGender() {
+        return gender;
+    }
 
-	/**
-	 * @param fidelityPoints the fidelityPoints to set
-	 */
-	public void setFidelityPoints(Integer fidelityPoints) {
-		this.fidelityPoints = fidelityPoints;
-	}
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
-	/**
-	 * @return the dailyPoints
-	 */
-	public Integer getDailyPoints() {
-		return dailyPoints;
-	}
+    public String getPushDeviceToken() {
+        return pushDeviceToken;
+    }
 
-	/**
-	 * @param dailyPoints the dailyPoints to set
-	 */
-	public void setDailyPoints(Integer dailyPoints) {
-		this.dailyPoints = dailyPoints;
-	}
-	
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
+    public void setPushDeviceToken(String pushDeviceToken) {
+        this.pushDeviceToken = pushDeviceToken;
+    }
 
-	/**
-	 * @param email the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getDeviceType() {
+        return deviceType;
+    }
 
-	/**
-	 * @return the cellphone
-	 */
-	public String getCellphone() {
-		return cellphone;
-	}
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
 
-	/**
-	 * @param cellphone the cellphone to set
-	 */
-	public void setCellphone(String cellphone) {
-		this.cellphone = cellphone;
-	}
+    public Integer getFidelityPoints() {
+        return fidelityPoints;
+    }
 
-	/**
-	 * @return the facebookId
-	 */
-	public String getFacebookId() {
-		return facebookId;
-	}
+    public void setFidelityPoints(Integer fidelityPoints) {
+        this.fidelityPoints = fidelityPoints;
+    }
 
-	/**
-	 * @param facebookId the facebookId to set
-	 */
-	public void setFacebookId(String facebookId) {
-		this.facebookId = facebookId;
-	}
+    public String getAvatar() {
+        return avatar;
+    }
 
-	/**
-	 * @return the gplusId
-	 */
-	public String getGplusId() {
-		return gplusId;
-	}
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 
-	/**
-	 * @param gplusId the gplusId to set
-	 */
-	public void setGplusId(String gplusId) {
-		this.gplusId = gplusId;
-	}
+    public Integer getDailyPoints() {
+        return dailyPoints;
+    }
 
-	/**
-	 * @return the gender
-	 */
-	public String getGender() {
-		return gender;
-	}
-
-	/**
-	 * @param gender the gender to set
-	 */
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	/**
-	 * @return the pushDeviceToken
-	 */
-	public String getPushDeviceToken() {
-		return pushDeviceToken;
-	}
-
-	/**
-	 * @param pushDeviceToken the pushDeviceToken to set
-	 */
-	public void setPushDeviceToken(String pushDeviceToken) {
-		this.pushDeviceToken = pushDeviceToken;
-	}
-
-	/**
-	 * @return the avatar
-	 */
-	public String getAvatar() {
-		return avatar;
-	}
-
-	/**
-	 * @param avatar the avatar to set
-	 */
-	public void setAvatar(String avatar) {
-		this.avatar = avatar;
-	}
-
-	/**
-	 * @return the deviceType
-	 */
-	public String getDeviceType() {
-		return deviceType;
-	}
-
-	/**
-	 * @param deviceType the deviceType to set
-	 */
-	public void setDeviceType(String deviceType) {
-		this.deviceType = deviceType;
-	}
-
-	public UserSimpleDTO getSuper(){
-		return new UserSimpleDTO(id, cpf, name);
-	}
-
+    public void setDailyPoints(Integer dailyPoints) {
+        this.dailyPoints = dailyPoints;
+    }
 }
